196169fa996803694d919b4fe36127b96d7dc34e
rujin2003 <rujindevkota@gmail.com>
2025-10-03 11:34:44 +0530

    project init

---
+++ VoiceJournal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VoiceJournal
+//
+//  Created by Rujin Devkota on 10/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VoiceJournal/VoiceJournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceJournalApp.swift
+//  VoiceJournal
+//
+//  Created by Rujin Devkota on 10/3/25.
+//
+
+import SwiftUI
+
+@main
+struct VoiceJournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
